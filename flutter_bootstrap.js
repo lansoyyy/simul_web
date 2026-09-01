0c59332836ca0703d7520d8c5dd4d23096a6b73c
neilpagsss14 <neilpagara50@gmail.com>
2026-09-01 10:58:40 +0800

    Deploy web build 2026-09-01_10-58 (v1.1.9)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"42d3d75a56efe1a2e9902f52dc8006099c45d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3800923630" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2832763393" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
